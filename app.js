eeaf3d2223f0922857b0b20bbda2b4adb58d8573
Oyinkolade <159939157+Oyinkolade@users.noreply.github.com>
2024-06-25 18:03:22 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+document.addEventListener('DOMContentLoaded',function(){
+
+    const list =document.querySelector('#movie-list ul');
+    const forms = document.forms;
+
+    //delete movie
+    list.addEventListener('click',(e)=>{
+        if(e.target.className == 'delete'){
+            const li = e.target.parentElement;
+            li.parentNode.removeChild(li);
+
+
+        }
+        
+    });
+
+    //add movie
+    const addForm = forms['add-movie'];
+    addForm.addEventListener('submit',function(e){
+    e.preventDefault();
+
+
+    //creating elements
+        const value = addForm.querySelector('input[type="text"]').value;
+        const li = document.createElement('li');
+        const movieName = document.createElement('span');
+        const deleteBtn = document.createElement('span');
+
+
+    //add text content
+        movieName.textContent = value;
+        deleteBtn.textContent = 'delete';
+
+
+    //add classes
+        movieName.classList.add('name');
+        deleteBtn.classList.add('delete');
+
+
+    //append to DOM
+        li.appendChild(movieName);
+        li.appendChild(deleteBtn);
+        list.appendChild(li);
+
+    });
+
+})
