33867c76a609eaa51e39ea4b91c2ed3d64551e7d
Oyinkolade <isholaoyinkolade@gmail.com>
2025-07-01 21:49:02 +0100

    Add files via upload

---
+++ Guessing Game Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Guessing_Game_2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an instance of the Game class
+            Game game = new Game();
+
+            // Set playAgain to true to start the game loop
+            game.playAgain = true;
+
+            while (game.playAgain)
+            {
+                // Ask the user which level they want to play
+                Console.WriteLine("Which Level do you want to attempt {Level 1, Level 2, or Level 3}");
+
+                // Collect the level the user wants to play
+                game.Levels = Console.ReadLine();
+
+                // Call the GetGame method to start the game based on the chosen level
+                game.GetGame();
+
+                // Ask the user if they want to play again
+                Console.WriteLine("Do you want to play again (yes/no)");
+
+                // Collect the response to play again
+                string playAgainResponse = Console.ReadLine();
+
+                // If the user says no, set playAgain to false, ending the game loop
+                if (playAgainResponse == "no")
+                {
+                    game.playAgain = false;
+                    Console.WriteLine("Thanks for playing the game!");
+                }
+            }
+        }
+    }
+}
---
+++ Guessing Game.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Guessing_Game_2
+{
+    // Create a class with relatable attributes
+    public class Game
+    {
+        // Attribute 1
+        public int RandomNumber { get; set; }
+
+        // Attribute 2
+        public int response { get; set; }
+
+        // Attribute 3
+        public string Levels { get; set; }
+
+        // Attribute 4
+        public List<int> listOfResponses { get; set; }
+
+        // Attribute 5
+        public bool playAgain { get; set; }
+
+        public Game()
+        {
+            listOfResponses = new List<int>();
+        }
+
+        // Recursive method to handle the game logic
+        public Game GuessNumber()
+        {
+            // Ask for user input (guess)
+            Console.WriteLine("Enter your guess:");
+
+            // Collect the user's response
+            this.response = Convert.ToInt32(Console.ReadLine());
+
+            // Add the response to the list
+            this.listOfResponses.Add(this.response);
+
+            // Compare response to RandomNumber
+            if (this.response < this.RandomNumber)
+            {
+                Console.WriteLine("Too Low");
+                // The Recursive method is call if the guess is incorrect
+                return GuessNumber();
+            }
+            else if (this.response > this.RandomNumber)
+            {
+                Console.WriteLine("Too High");
+                // The Recursive method is call if the guess is incorrect
+                return GuessNumber();
+            }
+            else
+            {
+                Console.WriteLine("Correct!");
+                Console.WriteLine("\nYour guesses were:");
+                for (int i = 0; i < this.listOfResponses.Count; i++)
+                {
+                    Console.WriteLine(this.listOfResponses[i]);
+                }
+                return this;
+            }
+        }
+
+        // Use a method that runs all the code for you
+        public Game GetGame()
+        {
+            // Set RandomNumber based on level, also give an instruction for each level
+            Random r = new Random();
+            if (this.Levels == "Level 1")
+            {
+                Console.WriteLine("Guess a number between 1 to 101");
+                this.RandomNumber = r.Next(1, 101);
+            }
+            else if (this.Levels == "Level 2")
+            {
+                Console.WriteLine("Guess a number between 1 to 301");
+                this.RandomNumber = r.Next(1, 301);
+            }
+            else if (this.Levels == "Level 3")
+            {
+                Console.WriteLine("Guess a number between 1 to 501");
+                this.RandomNumber = r.Next(1, 501);
+            }
+
+            return GuessNumber();
+        }
+    }
+}
