35644aec5ecf9de697b1a76e75d0dec74d8b1eba
Oyinkolade <isholaoyinkolade@gmail.com>
2025-07-01 21:58:02 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prime_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the starting number:");
+            int A = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the ending number:");
+            int B = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = A; i <= B; i++)
+            {
+                bool isPrime = true;
+
+                for (int j = 2; j <= i / 2; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+
+                if (isPrime && i > 1)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
